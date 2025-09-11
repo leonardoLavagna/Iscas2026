f7d0b2b9c6ae9e67577b2dfca05e7e44db7148b4
Leonardo Lavagna <leonardo.lavagna@uniroma1.it>
2025-09-11 12:17:23 +0200

    Create config.py

---
+++ config.py
@@ -0,0 +1,17 @@
+from qiskit_aer import Aer, AerSimulator
+from qiskit_aer.noise import NoiseModel
+from qiskit_ibm_runtime.fake_provider import FakeVigoV2
+
+# Constants
+shots = 1024
+marked_key = 5
+ks = list(range(1, 26))
+
+# Backends
+backend = AerSimulator()
+fake = FakeVigoV2()
+ideal_backend = AerSimulator(method="statevector")
+noise_model = NoiseModel.from_backend(fake)
+noisy_backend = AerSimulator(noise_model=noise_model,
+                             basis_gates=noise_model.basis_gates,
+                             coupling_map=fake.configuration().coupling_map)
